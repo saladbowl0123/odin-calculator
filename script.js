c52650f00e981690b48f89045980c24ae652ef7b
saladbowl0123 <saladbowl0123@gmail.com>
2024-06-12 14:31:21 +0800

    add operation and equals buttons functionality

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 const display = document.querySelector("#display");
 const nums = document.querySelector("#nums")
-const btn1 = document.querySelector("#btn-1");
+const ops = document.querySelector("#ops");
+const btnEquals = document.querySelector("#btn-equals");
+
+const calcStates = ["numA", "op", "numB", "result"];
+let calcState = calcStates[0];
 
 let numA;
-let operator;
+let op;
 let numB;
 
 function add (a, b) {
-  return a + b;
+  return parseFloat(a) + parseFloat(b);
 }
 
 function subtract (a, b) {
-  return a - b;
+  return parseFloat(a) - parseFloat(b);
 }
 
 function multiply (a, b) {
-  return a * b;
+  return parseFloat(a) * parseFloat(b);
 }
 
 function divide (a, b) {
-  return a / b;
+  return parseFloat(a) / parseFloat(b);
 }
 
-function operate (numA, operator, numB) {
-  switch (operator) {
+function operate (numA, op, numB) {
+  switch (op) {
     case "+":
       return add(numA, numB);
     case "-":
@@ -35,15 +39,53 @@ function operate (numA, operator, numB) {
   }
 }
 
-nums.addEventListener("click", appendDisplay);
-
-function appendDisplay (event) {
+nums.addEventListener("click", (event) => {
   let target = event.target;
-  if (display.textContent == "0") {
-    if (target.textContent != "0") {
+  switch (calcState) {
+    case calcStates[0]:
+    case calcStates[2]:
+      if (display.textContent == "0") {
+        if (target.textContent != "0") {
+          display.textContent = target.textContent;
+        }
+      } else {
+        display.textContent += target.textContent;
+      }
+      break;
+    case calcStates[1]:
       display.textContent = target.textContent;
-    }
-  } else {
-    display.textContent += target.textContent;
+      calcState = calcStates[2];
+      break;
+    case calcStates[3]:
+      display.textContent = target.textContent;
+      calcState = calcStates[0];
   }
-}
+});
+
+ops.addEventListener("click", (event) => {
+  let target = event.target;
+  switch (calcState) {
+    case calcStates[2]:
+      numB = display.textContent;
+      display.textContent = operate(numA, op, numB);
+      break;
+    default:
+      numA = display.textContent;
+  }
+  op = target.textContent;
+  calcState = calcStates[1];
+});
+
+btnEquals.addEventListener("click", (event) => {
+  switch (calcState) {
+    case calcStates[1]:
+    case calcStates[2]:
+      numB = display.textContent;
+      display.textContent = operate(numA, op, numB);
+      calcState = calcStates[3];
+      break;
+    case calcStates[3]:
+      numA = display.textContent;
+      display.textContent = operate(numA, op, numB);
+  }
+});
